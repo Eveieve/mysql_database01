ba8686bf6672b250e2c0f364666c9651269b69be
thatzcool <mycaptain0771@gmail.com>
2025-02-25 15:06:21 +0900

    day06_class_02

--- day06_ssg_class.sql
+++ day06_ssg_class.sql
@@ -19,8 +19,27 @@ SELECT ROUND(4.875,1);
 SELECT MOD(10,3) AS 나머지;
 
 -- 고객별 평균 주문 금액을 (백원 단위)로 반올림한 값을 구하시오 
-SELECT custid '고객번호',  ROUND(SUM(saleprice)/COUNT(*), -2) '평균금액'
+-- SELECT custid '고객번호',  ROUND(SUM(saleprice)/COUNT(*), -2) '평균금액'
+SELECT custid '고객번호',  ROUND(AVG(saleprice), -2) '평균금액'
 FROM Orders
 GROUP BY custid;
+
+SELECT TRIM('=' FROM '==FROM=FROM==');
+
+-- 한문자가 차지하는 바이트의 기준은 데이터베이스시스템에 따라 다르게 적용되는데
+-- UTF-8 환경에서는 알파벳 'a' 1byte,  한글 '가' =>  3바이트 로 저장된다. 
+-- 그러므로 순수한 문자의 수를 알기위해서는 CHAR_LENGTH(문자열) 사용한다. 
+-- 공백도 하나의 문자로 취급됩니다.
+
+SELECT LENGTH('SSSSSGGGG ');
+SELECT CHAR_LENGTH('신세계');
+SELECT LENGTH('신세계');
+
+SELECT * FROM BOOK;
+
+SELECT bookid, replace(bookname,'야구','농구') bookname
+FROM BOOK;
+
+
 
 
